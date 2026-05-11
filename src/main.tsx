283cc193e2abb1c021c87185fdd9fd052132dfbc
sgkim <sgim@proptier.co.kr>
2026-05-11 12:35:23 +0900

    [작업] TanStack Router 및 Query 환경 세팅
    - @tanstack/react-router, @tanstack/react-query 패키지 설치
    - @tanstack/router-devtools, @tanstack/react-query-devtools 추가
    - vite.config.ts에 devtools-vite, tanstackRouter 플러그인 연동
    - tsconfig.app.json strictNullChecks 활성화
    - 통합 Devtools (TanStackDevtools) 설정 및 루트 라우트 구성
    - main.tsx에 QueryClientProvider 및 RouterProvider 통합

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Create a client
+const queryClient = new QueryClient()
+
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Failed to find the root element')
+}
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { tanstackRouter  } from '@tanstack/router-plugin/vite'
+import { devtools } from '@tanstack/devtools-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    devtools(),
+   tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
