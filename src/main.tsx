461e16602850e4b6f45755c671e748f2ade7f2be
sgkim <sgim@proptier.co.kr>
2026-05-11 13:16:39 +0900

    [master] SCSS 설정 추가 및 샘플 컴포넌트 교체
    - sass-embedded 패키지 추가
    - SCSS 구조 생성 (variables, mixins, reset, global)
    - App 컴포넌트를 SCSS BEM 샘플로 교체
    - 기존 CSS 파일 삭제 (index.css, App.css)
    - .npmrc 사내 레지스트리 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './index.css';
+import './styles/global.scss';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
